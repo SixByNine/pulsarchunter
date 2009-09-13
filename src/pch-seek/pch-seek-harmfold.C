b738db843991836f2a4d18990a7aa7b0f3984bb9
Michael Keith <mkeith@pulsarastronomy.net>
2009-09-13 17:28:39 +1000

    New faster harmonic summing code
    Uses super-effient hsum code when harmonic list starts with powers of 2
    
    Added a python programme that replaces Andrew Lyne and his insane fortran skills!

--- src/pch-seek/pch-seek-harmfold.C
+++ src/pch-seek/pch-seek-harmfold.C
@@ -3,29 +3,75 @@
 #include <stdlib.h>
 #include <math.h>
 
+void pch_seek_harmfold_stupid(float* amplitudes, int ndat, int* folds, int ifolds, float** hfolds);
 
 /**
  * This is a 'stupid' harmonic folder that does each harmonic fold independantly.
  */
 float** pch_seek_harmfold_simple(float* amplitudes, int ndat, int* folds, int nfolds){
-	int ifold, foldval,f;
-	int samp;
+	int ifold;
 	float** hfolds;
-	float fac;
 
 	hfolds = (float**) malloc(sizeof(float*) * nfolds);
-	
 
+	// Loop over each harmonic fold
 	for (ifold=0; ifold < nfolds; ifold++){
-		foldval = folds[ifold];
+		// Make the array that stores the output
 		hfolds[ifold] = (float*) calloc(ndat,sizeof(float));
-		for (samp = 0; samp < ndat; samp++){
-			for (f=1; f <= foldval; f++){
-				hfolds[ifold][samp] += amplitudes[(int)((samp*f)/foldval)];
-			}
-		}
-		
+		pch_seek_harmfold_stupid(amplitudes,ndat,folds,ifold,hfolds);
+	}
+	return hfolds;
+}
+
+float** pch_seek_harmfold_smart(float* amplitudes, int ndat, int* folds, int nfolds){
+	int ifold,max_clever_folds;
+	float** hfolds;
+
+	hfolds = (float**) malloc(sizeof(float*) * nfolds);
+	// Make the array that stores the output
+	for (ifold=0; ifold < nfolds; ifold++)
+		hfolds[ifold] = (float*) calloc(ndat,sizeof(float));
+
+	// See if we have 2,4,8,16,... etc
+	for (ifold=0; ifold < nfolds; ifold++){
+		if (folds[ifold] != (int)pow(2,ifold+1) || ifold > 6)break;
+	}
+	max_clever_folds=ifold;
+	if(max_clever_folds > 0){
+		// if we did have powers of 2, we can do the 'clever' folding
+		// of AGL et al.
+		//printf("Smart_folding < %d\n",max_clever_folds);
+		pch_seek_harmfold_pow_two(amplitudes,ndat,hfolds,max_clever_folds);
 	}
 
+	// Any left over folds, we have to do the 'stupid' way
+	for (ifold=max_clever_folds; ifold < nfolds; ifold++){
+		//printf("Stupid_folding %d\n",ifold);
+		pch_seek_harmfold_stupid(amplitudes,ndat,folds,ifold,hfolds);
+	}
 	return hfolds;
+}
+
+
+
+
+void pch_seek_harmfold_stupid(float* amplitudes, int ndat, int* folds, int ifold, float** hfolds){
+	int foldval,f;
+	int samp,idx,last_idx;
+	float fac;
+
+	foldval = folds[ifold];
+// Loop over each sample in output
+	for (samp = 0; samp < ndat; samp++){
+		last_idx=0;
+		for (f=1; f <= foldval; f++){
+			// add samp/f 2samp/f 3samp/f 4samp/f ... fsamp/f
+			idx=(int)((samp*f)/(float)foldval + 0.5);
+			if (idx > 1 && idx != last_idx){
+				hfolds[ifold][samp] +=
+					amplitudes[idx];
+			}
+			last_idx=idx;
+		}
+	}
 }
